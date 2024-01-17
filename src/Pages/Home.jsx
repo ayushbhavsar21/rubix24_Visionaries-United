17f66ea8330bce7943e9a7d939603b9f66b07019
ayushbhavsar21 <ayush21bhavsar@gmail.com>
2024-01-17 22:17:36 +0530

    Added Router

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "../Components/Navbar"
+import Section1 from '../Section/Section1'
+import Section2 from '../Section/Section2'
+import Section4 from '../Section/Section4'
+
+function Home(){
+    return (
+        <>
+        <Navbar/>
+        <Section1/>
+        <Section2/>
+        <Section4/>
+        </>
+    )
+}
+export default Home;
