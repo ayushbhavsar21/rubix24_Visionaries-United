1d8d2072f36ab82185a3e875a25ae2dd332d698c
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-01-17 17:25:36 +0530

    added sections

---
+++ src/Section/Section2.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import section21 from '../assets/Section21.svg'
+import section22 from '../assets/section22.png'
+import section23 from '../assets/section23.png'
+import section24 from '../assets/section24.png'
+function Section2() {
+  return (
+    <div className='flex justify-around items-center w-full bg-primary h-[100vh]'>
+      <div className='w-[50%] relative left-16 top-20'>
+       <img src={section21} alt="" className='relative left-28 top-24 w-[45%]' />
+       <img src={section22} alt="" className='relative bottom-[350px] w-[130px] left-12'/>
+       <img src={section23} alt="" className='relative bottom-[110px] w-[130px] left-12'/>
+       <img src={section24} alt="" className='relative  w-[150px] bottom-[300px] left-10'/>
+      </div>
+      <div className='flex flex-col w-[35%] text-white font-playfair relative right-28 bottom-20 gap-4'>
+       <p className='text-[20px]'>ABOUT US</p>
+       <p className='text-[25px]'>Book 1:1 Sessions</p>
+       <p className='text-[20px]'>Creative & Proffesional Digital Mentors!</p>
+       <p className='text-[18px]'>Schedule a session instantly with a mentor and work with them directly on Teetors - whether it's for one-off tactical advice or multiple sessions towards a long-term goal.
+       </p>
+
+       <p className='text-[18px]'>
+       Schedule a session instantly with a mentor and work with them directly on Teetors - whether it's for one-off tactical advice or multiple sessions towards a long-term goal.
+       </p>
+       <button className='text-xs py-2 rounded-3xl bg-secondary w-[180px]'>EXPLORE MENTORS</button>
+      </div>
+    </div>
+  )
+}
+
+export default Section2
---
+++ src/Section/Section4.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import section41 from '../assets/section41.png'
+function Section4() {
+  return (
+    <div className='h-[130vh] bg-primary bg-cover bg-center flex flex-col  items-center gap-28' style={{ backgroundImage: `url(${section41})` }}>
+      <div className='text-center text-white'>
+     <p className='text-[40px] '>OUR TESTIMONIALS</p>
+     <p className='text-[50px] font-bold'>Wins from all around the</p>
+     <p className='text-[50px] font-bold'>Globe.</p>
+      </div>
+      <div className='flex justify-center gap-8  w-full items-center'>
+         <div className='w-[300px] h-[300px] bg-secondary shadow-lg shadow-white'>
+           <div className='flex flex-col gap-2 h-[100%] justify-center pl-4 text-white w-[280px]'>
+           <p>Ayush Bhavsar</p>
+            <p>12 September 2023</p>
+            <p>
+                
+            "Incredible programming course! Engaging content, clear explanations, and hands-on projects enhanced my coding skills. A must for anyone seeking practical knowledge and advancement in programming."
+            </p>
+           </div>
+            
+         </div>
+         <div className='w-[300px] h-[300px] bg-white shadow-lg shadow-white'>
+         <div className='flex flex-col gap-2 h-[100%] justify-center pl-4 text-gray-400 w-[280px]'>
+           <p>Satya Prakash Jena</p>
+            <p>18 December 2023</p>
+            <p>
+                
+            "Incredible programming course! Engaging content, clear explanations, and hands-on projects enhanced my coding skills. A must for anyone seeking practical knowledge and advancement in programming."
+            </p>
+           </div>
+         </div>
+         <div className='w-[300px] h-[300px] bg-white shadow-lg shadow-white'>
+         <div className='flex flex-col gap-2 h-[100%] justify-center pl-4 text-gray-400 w-[280px]'>
+           <p>Aviral Patel</p>
+            <p>17 January 2024</p>
+            <p>
+                
+            "Incredible programming course! Engaging content, clear explanations, and hands-on projects enhanced my coding skills. A must for anyone seeking practical knowledge and advancement in programming."
+            </p>
+           </div>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Section4
