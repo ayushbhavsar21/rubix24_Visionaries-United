08bbf206a9480d44a42433e5f77b5583cc573bef
ayushbhavsar21 <ayush21bhavsar@gmail.com>
2024-01-17 16:42:05 +0530

    Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
 import Navbar from './Components/Navbar'
-import SignIn from './Components/SignIn'
+// import SignIn from './Components/SignIn'
+// import MentorRegister from './Components/ṂentorRegister'
+// import MenteeRegister from './Components/MenteeRegister'
 function App() {
 
   return (
     <>
     
     <Navbar/>
-    <SignIn/>
+    {/* <SignIn/> */}
+    {/* <MenteeRegister/> */}
+    {/* <MentorRegister/> */}
     </>
   )
 }
