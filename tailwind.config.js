16c356799809e1a0badba0608886eac1c8ff6272
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-01-17 12:35:35 +0530

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030C0F",
+        
+        secondary: "#4A90E2",  
+       
+      },
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        dancing: ['Dancing Script', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
