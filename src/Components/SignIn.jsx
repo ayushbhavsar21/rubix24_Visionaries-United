0fee73a29a5687e68b286b3d473f16970b68dcff
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-18 10:42:54 +0530

    sign in added

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,10 +1,78 @@
 import Navbar from "./Navbar";
-// import { useState } from "react";
 import SignInclipart from "../assets/SignIn-Clipart(2).png"
 import line from "../assets/line.png"
 import Google from "../assets/googleLogo.svg"
+import React, { useState } from 'react'
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { auth, googleProvider } from '../config/firebase'
+import { collection, addDoc} from 'firebase/firestore';
+import { db } from '../config/firebase';
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 function SignIn() {
-    
+    const userCollectionRef = collection(db, "users");
+    const navigate = useNavigate();
+
+    const {login} = useAuth();
+
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleInput = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setUser({...user,
+            [name] : value
+        })
+    }
+
+    const handleSubmit = async(e)=>{
+
+        e.preventDefault();
+
+        try {
+            await signInWithEmailAndPassword(
+                auth,
+                user.email,
+                user.password
+              );
+
+              localStorage.setItem('userEmail', user.email);
+              localStorage.setItem('userRole', 'Mentee');
+              localStorage.setItem('userId', user.uid);
+
+              login();
+              navigate("/");
+      
+              console.log('User registered successfully:', userCredential.user);
+            
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+
+    const signInWithGoogle = async()=>{
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+
+            const user = result.user;
+            
+            localStorage.setItem('userEmail', user.email);
+            localStorage.setItem('userRole', 'Mentee');
+            localStorage.setItem('userId', user.uid);
+
+            login();
+            navigate("/");
+            
+        } catch (error) {
+            console.log(error);
+        }
+     }
     
     return (
         < >
@@ -20,21 +88,22 @@ function SignIn() {
                 <div className="lg:w-[50vw] md:w-[60vw] sm:w-[75vw] w-[95vw] flex flex-col lg:justify-start pt-2 lg:items-start items-center justify-center">
                     
                     <div className="rounded-[35px] bg-tertiary sm:w-[70%] w-[100%] h-[62vh] flex flex-col gap-1 p-8">
-                        
+                        <form onSubmit={handleSubmit}>
                         <div className="">
                         <div className="pl-1 pb-2">Email</div>
-                        <input className="rounded-lg h-[45px] w-[100%]" type="text" name="email"  />
+                        <input className="rounded-lg h-[45px] w-[100%]" type="text" name="email" value={user.email} onChange={ handleInput} />
                         </div>
                         
                         <div>
                         <div className="pl-1 pb-2">Password</div>
-                        <input className="h-[45px] w-[100%]  rounded-lg " type="text" name="password"  />
+                        <input className="h-[45px] w-[100%]  rounded-lg " type="text" name="password" value={user.password} onChange={handleInput}  />
                         </div>
                         <button  className="drop-shadow-[0_5px_5px_rgba(58,163,159,0.8)] px-4 py-3 w-[120px] bg-secondary text-white  rounded-3xl  self-center mt-4" >
                            SignIn
                         </button>
+                        </form>
                         <img className=" drop-shadow-2xl " src={line} alt="" />
-                        <button className="flex justify-center gap-4 rounded-3xl border-[2px] border-black items-center bg-white drop-shadow-2xl w-[80%] self-center p-2" >
+                        <button onClick={signInWithGoogle} className="flex justify-center gap-4 rounded-3xl border-[2px] border-black items-center bg-white drop-shadow-2xl w-[80%] self-center p-2" >
                             <img className="h-[32px]" src={Google} alt="Google" />
                             <p className="">Sign In with Google</p>
                         </button>
